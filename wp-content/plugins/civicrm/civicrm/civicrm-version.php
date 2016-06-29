19aeda3e06b99c0a1a3e7da7e4e9087d2224accd
Robert M. Parker <rparker@yamiko.ninja>
2016-06-29 08:28:49 -0700

    plugins

---
+++ wp-content/plugins/civicrm/civicrm/civicrm-version.php
@@ -0,0 +1,7 @@
+<?php
+function civicrmVersion( ) {
+  return array( 'version'  => '4.7.8',
+                'cms'      => 'Wordpress',
+                'revision' => '709650a014' );
+}
+
---
+++ wp-content/plugins/files/civicrm/templates_c/en_US/%%5A/5A6/5A68F7DC%%Demographics.tpl.php
@@ -0,0 +1,62 @@
+<?php /* Smarty version 2.6.27, created on 2016-06-13 01:47:02
+         compiled from CRM/Contact/Form/Edit/Demographics.tpl */ ?>
+<?php require_once(SMARTY_CORE_DIR . 'core.load_plugins.php');
+smarty_core_load_plugins(array('plugins' => array(array('block', 'crmScope', 'CRM/Contact/Form/Edit/Demographics.tpl', 1, false),)), $this); ?>
+<?php $this->_tag_stack[] = array('crmScope', array('extensionKey' => "")); $_block_repeat=true;smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], null, $this, $_block_repeat);while ($_block_repeat) { ob_start(); ?><div class="crm-accordion-wrapper crm-demographics-accordion collapsed">
+ <div class="crm-accordion-header">
+    <?php echo $this->_tpl_vars['title']; ?>
+
+  </div><!-- /.crm-accordion-header -->
+  <div id="demographics" class="crm-accordion-body">
+  <div class="form-item">
+        <span class="label"><?php echo $this->_tpl_vars['form']['gender_id']['label']; ?>
+</span>
+
+  <span class="value">
+        <?php echo $this->_tpl_vars['form']['gender_id']['html']; ?>
+
+        </span>
+  </div>
+  <div class="form-item">
+        <span class="label"><?php echo $this->_tpl_vars['form']['birth_date']['label']; ?>
+</span>
+        <span class="value"><?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "CRM/common/jcalendar.tpl", 'smarty_include_vars' => array('elementName' => 'birth_date')));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?></span>
+  </div>
+  <div class="form-item">
+       <?php echo $this->_tpl_vars['form']['is_deceased']['html']; ?>
+
+       <?php echo $this->_tpl_vars['form']['is_deceased']['label']; ?>
+
+  </div>
+  <div id="showDeceasedDate" class="form-item">
+       <span class="label"><?php echo $this->_tpl_vars['form']['deceased_date']['label']; ?>
+</span>
+       <span class="value"><?php $_smarty_tpl_vars = $this->_tpl_vars;
+$this->_smarty_include(array('smarty_include_tpl_file' => "CRM/common/jcalendar.tpl", 'smarty_include_vars' => array('elementName' => 'deceased_date')));
+$this->_tpl_vars = $_smarty_tpl_vars;
+unset($_smarty_tpl_vars);
+ ?></span>
+  </div>
+ </div><!-- /.crm-accordion-body -->
+</div><!-- /.crm-accordion-wrapper -->
+
+<?php echo '
+<script type="text/javascript">
+    showDeceasedDate( );
+    function showDeceasedDate( )
+    {
+        if ( cj("#is_deceased").is(\':checked\') ) {
+            cj("#showDeceasedDate").show( );
+        } else {
+    cj("#showDeceasedDate").hide( );
+         cj("#deceased_date").val(\'\');
+        }
+    }
+</script>
+'; ?>
+
+<?php $_block_content = ob_get_contents(); ob_end_clean(); $_block_repeat=false;echo smarty_block_crmScope($this->_tag_stack[count($this->_tag_stack)-1][1], $_block_content, $this, $_block_repeat); }  array_pop($this->_tag_stack); ?>
