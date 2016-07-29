c762703b6d5a0a6c498bedb41633485739769a10
Robert M. Parker <rparker@yamiko.ninja>
2016-07-29 02:43:12 -0700

    move global twig vars to another file

---
+++ wp-content/themes/Supertheme/app/bootstrap-theme.php
@@ -0,0 +1,38 @@
+<?php
+use Supertheme\WordPress\AccordionMenuWalker;
+use Supertheme\WordPress\DropDownMenuWalker;
+
+$data = [];
+
+$data['isLoggedIn'] = is_user_logged_in();
+$data['avatar'] = get_avatar(get_current_user_id(), 32);
+$data['editProfileLink'] = get_edit_user_link(get_current_user_id());
+$data['loginLink'] = wp_login_url();
+$data['logoutLink'] = wp_logout_url();
+$data['desktopMenu'] = wp_nav_menu([
+    'echo' => false,
+    'container' => false,
+    'items_wrap' => '<ul id="%1$s" class="%2$s dropdown menu hide-for-small-only float-right" data-dropdown-menu>%3$s</ul>',
+    'theme_location' => 'primary_menu',
+    'walker' => new DropDownMenuWalker(),
+]);
+$data['mobileMenu'] = wp_nav_menu([
+    'echo' => false,
+    'container' => false,
+    'items_wrap' => '<ul id="%1$s" class="%2$s vertical menu" data-accordion-menu>%3$s</ul>',
+    'theme_location' => 'primary_menu',
+    'walker' => new AccordionMenuWalker(),
+]);
+$data['footerMenu'] = wp_nav_menu([
+    'echo' => false,
+    'container' => false,
+    'items_wrap' => '<ul id="%1$s" class="%2$s vertical medium-horizontal menu">%3$s</ul>',
+    'theme_location' => 'footer_menu',
+]);
+$data['layout'] = get_field('layout') ?: "one";
+
+ob_start();
+dynamic_sidebar( 'right_sidebar' );
+$data['right_sidebar'] = ob_get_clean();
+
+return $data;
--- wp-content/themes/Supertheme/index.php
+++ wp-content/themes/Supertheme/index.php
@@ -1,6 +1,4 @@
 <?php
-use Supertheme\WordPress\AccordionMenuWalker;
-use Supertheme\WordPress\DropDownMenuWalker;
 require_once __DIR__.'/app/bootstrap.php';
 
 // get services
@@ -9,32 +7,7 @@ require_once __DIR__.'/app/bootstrap.php';
 $twig = $container->get("twig.environment");
 
 // preg global twig data
-$data = [];
-$data['isLoggedIn'] = is_user_logged_in();
-$data['avatar'] = get_avatar(get_current_user_id(), 32);
-$data['editProfileLink'] = get_edit_user_link(get_current_user_id());
-$data['loginLink'] = wp_login_url();
-$data['logoutLink'] = wp_logout_url();
-$data['desktopMenu'] = wp_nav_menu([
-    'echo' => false,
-    'container' => false,
-    'items_wrap' => '<ul id="%1$s" class="%2$s dropdown menu hide-for-small-only float-right" data-dropdown-menu>%3$s</ul>',
-    'theme_location' => 'primary_menu',
-    'walker' => new DropDownMenuWalker(),
-]);
-$data['mobileMenu'] = wp_nav_menu([
-    'echo' => false,
-    'container' => false,
-    'items_wrap' => '<ul id="%1$s" class="%2$s vertical menu" data-accordion-menu>%3$s</ul>',
-    'theme_location' => 'primary_menu',
-    'walker' => new AccordionMenuWalker(),
-]);
-$data['footerMenu'] = wp_nav_menu([
-    'echo' => false,
-    'container' => false,
-    'items_wrap' => '<ul id="%1$s" class="%2$s vertical medium-horizontal menu">%3$s</ul>',
-    'theme_location' => 'footer_menu',
-]);
+$data = require_once __DIR__ . '/app/bootstrap-theme.php';
 
 // get content rows
 $rows = [];
