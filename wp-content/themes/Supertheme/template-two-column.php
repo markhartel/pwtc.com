4b2e956f6e2ac773d44cb2580448f113d4a17c9e
Robert M. Parker <rparker@yamiko.ninja>
2016-07-29 02:45:31 -0700

    two column view

---
+++ wp-content/themes/Supertheme/template-two-column.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template Name: Two Columns
+ *
+ */
+require_once __DIR__.'/app/bootstrap.php';
+
+// get services
+/** @var \Symfony\Component\DependencyInjection\Container $container */
+/** @var Twig_Environment $twig */
+$twig = $container->get("twig.environment");
+
+// preg global twig data
+$data = require_once __DIR__ . '/app/bootstrap-theme.php';
+
+// get content rows
+$rows = [];
+while(have_rows('content_rows')) {
+    the_row();
+    $rows[] = $twig->render('rows/'.get_row_layout().'.html.twig');
+}
+$data['rows'] = $rows;
+
+// render
+echo $twig->render('two-column.html.twig', $data);
