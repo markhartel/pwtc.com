d945ccd6d9ab2be42a3e6a2421e858b8ea815420
Robert M. Parker <rparker@yamiko.ninja>
2016-07-29 02:45:06 -0700

    widgets

--- wp-content/themes/Supertheme/web/scripts/app.js
+++ wp-content/themes/Supertheme/web/scripts/app.js
@@ -1,3 +1,10 @@
+stLight.options({
+    publisher: "31abfba6-0978-4139-8479-d6e96f61d25f",
+    doNotHash: true,
+    doNotCopy: true,
+    hashAddressBar: false
+});
+
 jQuery(function() {
     jQuery(document).foundation();
     jQuery('.fancybox').fancybox();
