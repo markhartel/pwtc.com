079358dd6647640532f216783efdb0a3ae62fce7
diaboloshogunate <rparker@kodama.design>
2017-02-13 04:04:16 -0800

    fix length bug

--- wp-content/themes/Supertheme/single-scheduled_rides.php
+++ wp-content/themes/Supertheme/single-scheduled_rides.php
@@ -19,26 +19,21 @@ if(get_field('attach_map')) {
     $maps = [];
     foreach (get_field('maps') as $map) {
         $map_id = $map;
-
-        // set length to the lowest length
-        if (!$length) {
-            $length = get_field('length', $map_id);
-        } else if ($length && get_field('length', $map_id) < $length) {
+        if($length) {
+            $length = min(get_field('length', $map_id), $length);
+        } else {
             $length = get_field('length', $map_id);
         }
-
-        // set max length to the highest max length
-        if (!$maxLength) {
-            $maxLength = get_field('max_length', $map_id);
-        } else if ($maxLength && get_field('max_length', $map_id) < $maxLength) {
+        if($maxLength) {
+            $maxLength = max(get_field('max_length', $map_id), $maxLength);
+        } else {
             $maxLength = get_field('max_length', $map_id);
         }
-
         $terrain = array_merge($terrain, get_field('terrain', $map_id));
         $maps = array_merge($maps, get_field('maps', $map_id));
     }
 }
-$data['terrain'] = isset($terrain) ? $terrain : get_field('terrain');
+$data['terrain'] = isset($terrain) ? array_unique($terrain) : get_field('terrain');
 $data['length'] = isset($length) ? $length : get_field('length');
 $data['max_length'] = isset($maxLength) ? $maxLength : get_field('max_length');
 $data['maps'] = isset($maps) ? $maps : false;
