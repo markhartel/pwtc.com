1e60b35c3b32b2be9118071bdf10cd8bd92044a2
diaboloshogunate <rparker@kodama.design>
2017-02-13 19:39:02 -0800

    adjust ride length range calculations

--- wp-content/themes/Supertheme/single-scheduled_rides.php
+++ wp-content/themes/Supertheme/single-scheduled_rides.php
@@ -19,20 +19,27 @@ if(get_field('attach_map')) {
     $maps = [];
     foreach (get_field('maps') as $map) {
         $map_id = $map;
+
         if($length) {
             $length = min(get_field('length', $map_id), $length);
         } else {
             $length = get_field('length', $map_id);
         }
+
+
         if($maxLength) {
-            $maxLength = max(get_field('max_length', $map_id), $maxLength);
+            $maxLength = max(get_field('max_length', $map_id), $maxLength, $length);
         } else {
-            $maxLength = get_field('max_length', $map_id);
+            $maxLength = get_field('max_length', $map_id) ?: $length;
         }
+
         $terrain = array_merge($terrain, get_field('terrain', $map_id));
         $raw_map = get_field('maps', $map_id);
         $raw_map[0]['title'] = $map->post_title;
         $maps = array_merge($maps, $raw_map);
+    }
+    if($length == $maxLength) {
+        $maxLength = null;
     }
 }
 $data['terrain'] = isset($terrain) ? array_unique($terrain) : get_field('terrain');
