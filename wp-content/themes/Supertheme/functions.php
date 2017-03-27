9781a2750299d7b374ed41c34840e0d8b8fe9eae
diaboloshogunate <rparker@kodama.design>
2017-03-26 21:24:08 -0700

    adjust role permissions

--- wp-content/themes/Supertheme/functions.php
+++ wp-content/themes/Supertheme/functions.php
@@ -30,24 +30,53 @@ add_action('init', function() {
     add_role('current_member', 'Current Member', ['read' => false]);
     add_role('expired_member', 'Expired Member', ['read' => false]);
     add_role('ride_leader', 'Ride Leader', ['read' => true]);
-    add_role('ride_captain', 'Ride Captain', [
-        'access_uploaded_files' => true,
-        'delete_others_rides' => true,
-        'delete_private_rides' => true,
-        'delete_published_rides' => true,
-        'delete_rides' => true,
-        'edit_others_rides' => true,
-        'edit_private_rides' => true,
-        'edit_published_rides' => true,
-        'edit_rides' => true,
-        'publish_rides' => true,
-        'read' => true,
-        'read_private_rides' => true,
-        'register_for_events' => true,
-        'sign_civicrm_petition' => true,
-        'upload_files' => true,
-        'view_event_info' => true,
-    ]);
+    add_role('ride_captain', 'Ride Captain', ['read' => true]);
+
+    $ride_captain_role = get_role('ride_captain');
+    $additional_ride_captain_roles = [
+        'access_uploaded_files',
+        'delete_others_rides',
+        'delete_private_rides',
+        'delete_published_rides',
+        'delete_rides',
+        'edit_others_rides',
+        'edit_private_rides',
+        'edit_published_rides',
+        'edit_rides',
+        'publish_rides',
+        'read_private_rides',
+        'register_for_events',
+        'sign_civicrm_petition',
+        'upload_files',
+        'view_event_info',
+        'access_all_custom_data',
+        'access_civimail_subscribe_unsubscribe_pages',
+        'make_online_contributions',
+        'profile_create',
+        'profile_edit',
+        'profile_view',
+        'view_public_civimail_content',
+    ];
+    foreach($additional_ride_captain_roles as $role) {
+        $ride_captain_role->add_cap($role);
+    }
+
+    $editor_role = get_role('editor');
+    $additional_editor_roles = [
+        'delete_others_rides',
+        'delete_private_rides',
+        'delete_published_rides',
+        'delete_rides',
+        'edit_others_rides',
+        'edit_private_rides',
+        'edit_published_rides',
+        'edit_rides',
+        'publish_rides',
+        'read_private_rides',
+    ];
+    foreach($additional_editor_roles as $role) {
+        $editor_role->add_cap($role);
+    }
 
     register_post_type('ride_maps', [
         'public' => true,
