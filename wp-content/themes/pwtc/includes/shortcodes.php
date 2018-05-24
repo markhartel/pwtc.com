3442afc04db1cf5eb103a83609e6af3d8b466ec3
Robert Parker <rparker@kodama.design>
2018-05-23 21:24:18 -0700

    remove debug statement

--- wp-content/themes/pwtc/includes/shortcodes.php
+++ wp-content/themes/pwtc/includes/shortcodes.php
@@ -1,6 +1,5 @@
 <?php
 add_shortcode('pwtc_renew', function() use($twig) {
-    var_dump(is_user_logged_in());
     return $twig->render('shortcodes/renew.html.twig', [
         'is_logged_in' => is_user_logged_in(),
     ]);
