f0f69d429a611067a09832a7914e20111a52fcaa
Robert Parker <rparker@kodama.design>
2018-06-06 23:02:02 -0700

    new key

--- wp-content/themes/pwtc/includes/options-page.php
+++ wp-content/themes/pwtc/includes/options-page.php
@@ -7,8 +7,8 @@ if(function_exists('acf_add_options_page')) {
         'capability' 	=> 'edit_posts',
         'redirect' 	=> false
     ));
-
-    add_action('acf/init', function () {
-        acf_update_setting('google_api_key', 'AIzaSyCwHd26TxzmwwtDpDL1BK8iKmnDlIo5Ukg');
-    });
 }
+
+add_action('acf/init', function () {
+    acf_update_setting('google_api_key', 'AIzaSyB869ZHXQOQYCYEDxpDkMcD7BkTUpCRVeQ');
+});
