1dd864aaeecfe5b1169e97042418095eb7c5324e
markhartel <mark_hartel@hotmail.com>
2021-06-29 15:34:38 -0700

    Update single-ride_maps.php

--- wp-content/themes/pwtc2/resources/single-ride_maps.php
+++ wp-content/themes/pwtc2/resources/single-ride_maps.php
@@ -1,1 +1,17 @@
+<?php
+require_once __DIR__.'/../app/bootstrap.php';
+use Timber\Timber;
+use Timber\PostQuery;
 
+/** @var $timber Timber */
+$timber = $container->get('timber');
+$data = $timber::get_context();
+$data['post'] = $timber::get_post();
+
+$data['terrain'] = get_field('terrain');
+$data['length'] = get_field('length');
+$data['max_length'] = get_field('max_length');
+$data['current_url'] = get_permalink();
+
+// render
+$timber->render('pages/single-ride_map.html.twig', $data);
