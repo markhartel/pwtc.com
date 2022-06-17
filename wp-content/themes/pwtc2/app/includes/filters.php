910750a148f56e501e5a5c4f804c51219d54b67f
markhartel <mark_hartel@hotmail.com>
2022-06-17 12:46:25 -0700

    Update filters.php

--- wp-content/themes/pwtc2/app/includes/filters.php
+++ wp-content/themes/pwtc2/app/includes/filters.php
@@ -13,14 +13,14 @@ add_action('init', function() {
 add_filter('login_redirect', function ($redirect_to, $request, $user) {
     if ( isset( $user->roles ) && is_array( $user->roles ) ) {
         if ( in_array( 'administrator', $user->roles ) ) {
-            //error_log( 'login_redirect: administrator, redirect to: ' . $redirect_to);
+            //error_log( 'login_redirect: administrator, redirect to ' . $redirect_to);
             return $redirect_to;
         } else {
-            //error_log( 'login_redirect: not administrator, redirect to: ' . home_url());
+            //error_log( 'login_redirect: not administrator, change redirect from ' . $redirect_to . ' to ' . home_url());
             return home_url();
         }
     } else {
-        //error_log( 'login_redirect: role not available, redirect to: ' . $redirect_to);
+        //error_log( 'login_redirect: role not available, redirect to ' . $redirect_to);
         return $redirect_to;
     }
 }, 999999, 3);
