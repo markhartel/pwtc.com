e4a02cc9283698c2d1bf91a916d2e72475fcad51
markhartel <mark_hartel@hotmail.com>
2025-03-16 16:09:24 -0700

    Update author.php

--- wp-content/themes/pwtc2/resources/author.php
+++ wp-content/themes/pwtc2/resources/author.php
@@ -1,1 +1,11 @@
+<?php
+require_once __DIR__.'/../app/bootstrap.php';
+use Timber\Timber;
+use Timber\PostQuery;
 
+/** @var $timber Timber */
+$timber = $container->get('timber');
+$data = $timber::get_context();
+
+// render
+$timber->render('pages/author.html.twig', $data);
