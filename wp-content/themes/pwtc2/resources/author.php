938413eddef3b61952a5aa727fbd47fe48992af2
markhartel <mark_hartel@hotmail.com>
2025-03-19 16:52:06 -0700

    Update author.php

--- wp-content/themes/pwtc2/resources/author.php
+++ wp-content/themes/pwtc2/resources/author.php
@@ -7,5 +7,9 @@ use Timber\PostQuery;
 $timber = $container->get('timber');
 $data = $timber::get_context();
 
+$curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+
+$data['nickname'] = $curauth->nickname;
+    
 // render
 $timber->render('pages/author.html.twig', $data);
