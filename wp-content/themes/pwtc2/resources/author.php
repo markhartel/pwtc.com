7096be1a0500956b1ab5c0e1c2dc06009f9c2c6f
markhartel <mark_hartel@hotmail.com>
2025-03-19 17:10:41 -0700

    Update author.php

--- wp-content/themes/pwtc2/resources/author.php
+++ wp-content/themes/pwtc2/resources/author.php
@@ -10,6 +10,8 @@ $data = $timber::get_context();
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 
 $data['nickname'] = $curauth->nickname;
+$data['first_name'] = $curauth->first_name;
+$data['last_name'] = $curauth->last_name;
     
 // render
 $timber->render('pages/author.html.twig', $data);
